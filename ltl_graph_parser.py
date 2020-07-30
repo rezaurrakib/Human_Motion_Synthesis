d275f84aac2f53e63fc41617143f8ba57cf616d3
reza <reza.rahman@tum.de>
2020-07-30 13:28:53 +0200

    Path traversal and action string retrieval is done.

--- ltl_graph_parser.py
+++ ltl_graph_parser.py
@@ -12,16 +12,18 @@ _status_ = "Dev"
 class LTLGraphCreation():
     def __init__(self, file_data):
         buf = io.StringIO(file_data)
-        self.graph = {} # Stores all node-edge information from the ltl graph
+        self.graph = {}  # Stores all node-edge information from the ltl graph
         self.graph_center = None
         self.parsing_buffer(buf)
         self.trv_ftime = None
         self.dead_nodes = []
-          
+        self.total_vertex = 0
+
     def parsing_buffer(self, buf):
         self.lines = buf.readlines()
         for line in self.lines:
-            if (("->" in line) and ("[label=<" in line)): # Contain edge info between nodes
+            if (("->" in line) and ("[label=<" in line)):  # Contain edge info between nodes
+                # print("Line: ", line)
                 pattern = "<(.*?)>"
                 edges = re.search(pattern, line).group(1)
                 edges = edges.replace("&amp;", "&")
@@ -31,98 +33,83 @@ class LTLGraphCreation():
                 self.store_graph_info(nodes, edges)
 
             elif ("peripheries" in line):
-                self.graph_center = int(re.search(r'\d+', line).group()) # Only take the first node info
-    
-    def store_graph_info(self, nodes, edges):
-        # Source node is empty
+                self.graph_center = int(re.search(r'\d+', line).group())  # Only take the first node info
+
+        self.total_vertex = len(self.graph.keys())
+
+    def store_graph_info(self, nodes, edge):
+        # Source node is empty only
         if self.graph.get(nodes[0]) == None:
             temp = {}
             temp["incoming"] = []
             temp["outgoing"] = []
+            temp["edge_info"] = {}  # dict for node -> edge info for tracing the action along the path
             self.graph[nodes[0]] = temp
-        
-        # Destination node is empty but source node already exists 
+
+        # Destination node is empty but source node already exists
         if self.graph.get(nodes[1]) == None:
             temp = {}
             temp["incoming"] = []
             temp["outgoing"] = []
+            temp["edge_info"] = {}  # dict for node -> edge info for tracing the action along the path
             self.graph[nodes[1]] = temp
-        
+
         self.graph.get(nodes[0])["outgoing"].append(nodes[1])
         self.graph.get(nodes[1])["incoming"].append(nodes[0])
-    
+        self.graph.get(nodes[0])["edge_info"][nodes[1]] = edge  # e.g., {0: {'edge_info':{1: 'action_str'}}}
+
     def graph_print(self):
         print(self.graph)
-        print("No. of Vertices : ", len(self.graph.keys()))
+        print("Total nodes are : ", len(self.graph.keys()))
         print("The Graph center: ", self.graph_center)
-    
+
+    def get_action(self, src_node, dst_node):
+        edge_str = self.graph.get(src_node)["edge_info"][dst_node] # edge_str: edge information as string
+        print(edge_str)
+        actn_strings = re.findall(r"[!A-Z\d]+", edge_str)  # only get the actions (uppercase chars) + digits
+        actions = []
+        reg_exp = re.compile(r'^[A-Z]+\d*')  # Always starts with alpha char, followed by 0 or more digits
+        for atm in actn_strings:
+            if '!' not in atm and reg_exp.match(atm):
+                actions.append(atm)
+
+        actions = list(set(actions))  # convert the list as a unique actions list
+        # print("Actions: ", actions)
+        return actions
+
     def check_dead_nodes(self):
         for k in self.graph.keys():
             sz = len(self.graph[k]["outgoing"])
             if ((sz == 1) and (self.graph[k]["outgoing"][0] == k)):
                 self.dead_nodes.append(k)
-        
+
         if (len(self.dead_nodes) > 0):
             print("Dead nodes exist in the Graph.\nDead nodes are: ")
             for i in self.dead_nodes:
                 print(i)
-    
+
     def is_dead(self, u):
         for i in self.dead_nodes:
             if u == i:
                 return True
         return False
 
-    '''
-    def print_all_paths(self, src):
-        sz = len(self.graph.keys())
-        visited = [False for _ in range(sz)]
-        path = [] # List to store paths
-        # self.trv_ftime = True
-        self.recurse_pathfinder(src, visited, path)
-    
-    def recurse_pathfinder(self, u, visited, path):
-        # Mark the current node
-        visited[u] = True
-        path.append(u)
-        
-        # if u == d and self.trv_ftime == False:
-        #    print("Path: ", path)
-            
-        # Check in terms of dead nodes, If found then return back to start node.
-        if self.is_dead(u):
-            print("Enter into dead Node ", u)
-            path.append(0)
-            print("The updated path is : ", path)
-        
-        else:
-            # self.trv_ftime = False
-            edges = self.graph[u]["outgoing"]
-            # print("edges --> ", edges)
-            for e in edges:
-                if visited[e] == False:
-                    self.recurse_pathfinder(e, visited, path)
-        
-        # Remove current node from path[] and mark it as unvisited 
-        path.pop() 
-        visited[u]= False
-    '''
-
-    def recurse_traversal(self, u, path, p_len=20):
+    def recurse_traversal(self, cur_node, path, actions, p_len=20):
         if len(path) > p_len:
-            print(path)
+            print("Traversal : ", path)
+            print("Actions taken: ", actions)
             return
 
-        edges = self.graph[u]["outgoing"]
+        edges = self.graph[cur_node]["outgoing"]
 
-        while (True):
+        while True:
             next_node = random.choice(edges)
             # print("Current randomly chosen node: ", next_node)
-            if self.is_dead(next_node) or u == next_node:
+            if self.is_dead(next_node) or cur_node == next_node:
                 continue
             else:
                 break
 
         path.append(next_node)
-        self.recurse_traversal(next_node, path)
-
+        actions.append(self.get_action(cur_node, next_node))
+        self.recurse_traversal(next_node, path, actions)
